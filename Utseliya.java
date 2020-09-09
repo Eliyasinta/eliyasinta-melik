9e7bf39c493655744452ad86a2b77a019a1552c8
Eliyasinta <70988334+Eliyasinta@users.noreply.github.com>
2020-09-09 11:52:14 +0700

    Add files via upload

---
+++ Utseliya.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utseliya;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Utseliya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+        
+        System.out.println(alfabet);
+        System.out.println(""+alfabet.substring(4,5));
+        System.out.println(""+alfabet.substring(11,12));
+        System.out.println(""+alfabet.substring(8,9));
+        System.out.println(""+alfabet.substring(24,25));
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(18,19));
+        System.out.println(""+alfabet.substring(8,9));
+        System.out.println(""+alfabet.substring(13,14));
+        System.out.println(""+alfabet.substring(19,20));
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(12,13));
+        System.out.println(""+alfabet.substring(4,5));
+        System.out.println(""+alfabet.substring(11,12));
+        System.out.println(""+alfabet.substring(8,9));
+        System.out.println(""+alfabet.substring(10,11));
+        System.out.println(""+alfabet.substring(7,8));
+        System.out.println(""+alfabet.substring(0,1));
+        System.out.println(""+alfabet.substring(17,18));
+        System.out.println(""+alfabet.substring(20,21));
+        System.out.println(""+alfabet.substring(13,14));
+        
+        System.out.println(""+alfabet.substring(4,5)+alfabet.substring(11,12)+alfabet.substring(8,9)+alfabet.substring(24,25)+alfabet.substring(0,1)+alfabet.substring(18,19)+alfabet.substring(8,9)+alfabet.substring(13,14)+alfabet.substring(19,20)+alfabet.substring(0,1)+alfabet.substring(27,28)+alfabet.substring(12,13)+alfabet.substring(4,5)+alfabet.substring(11,12)+alfabet.substring(8,9)+alfabet.substring(10,11)+alfabet.substring(27,28)+alfabet.substring(7,8)+alfabet.substring(0,1)+alfabet.substring(17,18)+alfabet.substring(20,21)+alfabet.substring(13,14));
+                          
+    }
+    
+}
